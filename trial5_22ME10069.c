96cd0bb241ba8b2d01710cd3486809c169f0b54d
Rakshit07-lab <112808189+Rakshit07-lab@users.noreply.github.com>
2022-11-18 16:51:04 +0530

    Add files via upload

---
+++ trial5_22ME10069.c
@@ -0,0 +1,38 @@
+/*
+Name: Rakshit Raj
+Roll no: 22ME10069
+Group no:
+Description: Program to arrange 3 numbers in non-decreasing order
+*/
+
+#include<stdio.h>
+void swap(int *x, int *y)
+{
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+main()
+{
+    int a,b,c;
+    printf("Enter three integers\n");
+    scanf("%d%d%d",&a,&b,&c);
+
+    if(a>b)
+    {
+        swap(&a,&b);
+    }
+
+    if(a>c)
+    {
+        swap(&a,&c);
+    }
+
+    if(b>c)
+    {
+        swap(&b,&c);
+    }
+
+    printf("%d<%d<%d",a,b,c);
+}
